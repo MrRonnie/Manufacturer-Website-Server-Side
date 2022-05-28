c7da170b189efe845e529be0c8ce6c0d8cab6607
MrRonnie <ronymaza55@gmail.com>
2022-05-28 20:40:07 +0600

    set uri in mongodb

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Middle ware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9syuk.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
 
 app.get("/", (req, res) => {
   res.send("Hello From GearX Manufacture");
